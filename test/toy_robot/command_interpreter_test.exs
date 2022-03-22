ac2f22f96aca2728a2be02fe05e741f0d83bbf63
Anh Mac <anhmv91@gmail.com>
2022-03-22 13:56:11 +0700

    test: more test cases

--- test/toy_robot/command_interpreter_test.exs
+++ test/toy_robot/command_interpreter_test.exs
@@ -26,13 +26,16 @@ defmodule ToyRobot.CommandInterpreterTest do
   end
 
   test "marks invalid commands as invalid" do
-    commands = ["SPIN", "TWIRL", "EXTERMINATE"]
+    commands = ["SPIN", "TWIRL", "EXTERMINATE", "PLACE 1, 2, NORTH", "move", "MovE"]
     output = commands |> CommandInterpreter.interpret
 
     assert output == [
       {:invalid, "SPIN"},
       {:invalid, "TWIRL"},
-      {:invalid, "EXTERMINATE"}
+      {:invalid, "EXTERMINATE"},
+      {:invalid, "PLACE 1, 2, NORTH"},
+      {:invalid, "move"},
+      {:invalid, "MovE"}
     ]
   end
 end
